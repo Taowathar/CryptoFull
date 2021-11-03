97f695b2a1ae67e245eade7df010bde070200a1e
Taowathar <taowathar@gmail.com>
2021-11-03 11:08:02 +0100

    Initalize CI/CD workflow

--- CryptoFull/Program.cs
+++ CryptoFull/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CryppitBackend.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CryppitBackend
 {
@@ -13,7 +16,13 @@ namespace CryppitBackend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var service = (IServiceScopeFactory)host.Services.GetService(typeof(IServiceScopeFactory));
+            using (var db = service.CreateScope().ServiceProvider.GetService<AppDbContext>())
+            {
+                db.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +31,7 @@ namespace CryppitBackend
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
     }
 }
